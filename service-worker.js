4e1b6540346768b3197b56202c1e1216658dc0e6
biomech-tools <akihiroazuma@hotmail.com>
2026-05-18 23:20:32 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,16 @@
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open("cct-cache").then((cache) => {
-      return cache.addAll(["./index.html", "./"]);
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
+      ]);
     })
   );
 });
-
 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
